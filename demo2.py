6592e04255d62f889bc5688a87896c5e4ff20d18
awaelchli <adrian.waelchli@students.unibe.ch>
2017-09-14 17:52:05 +0200

    demo

--- demo2.py
+++ demo2.py
@@ -6,11 +6,12 @@ from torch.autograd import Variable
 
 input = Variable(torch.rand(3, 3, 100, 400)).cuda()
 
-model = ConvLSTM(3, [64, 2], 7, bias=True)
+model = ConvLSTM(3, [64, 2], 7, step=1, effective_step=[1], bias=True)
 model.cuda()
 
 for i in range(1000):
     model.zero_grad()
     output, _ = model.forward(input)
+    print(output)
     loss = output.mean()
     loss.backward()
